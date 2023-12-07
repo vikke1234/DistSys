c83039f678dcfddb5f2e4338d0e4a451025086fa
Viktor <viktor.horsmanheimo@gmail.com>
2023-12-07 22:54:10 +0200

    Add basic game
    Game consists of simple movement with generated coins,
    when collected these coins increase the score of the player.

--- src/main/java/org/uoh/distributed/game/Foobar.java
+++ src/main/java/org/uoh/distributed/game/Foobar.java
@@ -1,8 +1,13 @@
 package org.uoh.distributed.game;
-import lombok.RequiredArgsConstructor;
+
+import org.uoh.distributed.peer.game.Coin;
+import org.uoh.distributed.peer.game.GlobalView;
+import org.uoh.distributed.peer.game.Player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -15,40 +20,31 @@ public class Foobar extends JFrame {
     private final int gridSize = 10; // size of the grid
     private final int cellSize = 40; // pixel size of each grid cell
 
-    private static class Game extends JPanel implements KeyListener {
-        private final int cellSize;
-        private final int gridSize;
-        private int playerX = 0;
-        private int playerY = 0;
+    private static class Game extends JPanel implements KeyListener, ActionListener {
+        private Player localPlayer;
+        private GlobalView map;
 
-        private Food food;
+        /** Implements the refresh rate) */
+        private Timer timer;
 
         public Game(int cellSize, int gridSize) {
-            this.cellSize = cellSize;
-            this.gridSize = gridSize;
+            localPlayer = new Player("testing", 0, 0);
+            map = new GlobalView(gridSize, gridSize, cellSize);
+            map.addObject(localPlayer);
+
             setFocusable(true);
             requestFocus();
             addKeyListener(this);
-            food = Food.generateFood(gridSize, gridSize);
+            Coin coin = new Coin(5, 5);
+            map.addObject(coin);
+            timer = new Timer(50, this);
+            timer.start();
         }
 
         @Override
         public void paint(Graphics g) {
             super.paint(g);
-            for (int i = 0; i < gridSize; i++) {
-                for (int j = 0; j < gridSize; j++) {
-                    g.drawRect(i * cellSize, j * cellSize, cellSize, cellSize);
-                }
-            }
-            if (playerX == food.getX() && playerY == food.getY()) {
-                food = Food.generateFood(gridSize, gridSize);
-            }
-            drawFood(g, food);
-            g.fillOval(playerX * cellSize, playerY * cellSize, cellSize, cellSize);
-        }
-
-        private void drawFood(Graphics g, Food food) {
-            g.fillRect(food.getX() * cellSize, food.getY() * cellSize, cellSize, cellSize);
+            map.drawMap(g);
         }
 
         @Override
@@ -58,32 +54,25 @@ public class Foobar extends JFrame {
 
         @Override
         public void keyPressed(KeyEvent e) {
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_UP:
-                    playerY = Math.max(playerY - 1, 0);
-                    break;
-                case KeyEvent.VK_DOWN:
-                    playerY = Math.min(playerY + 1, gridSize - 1);
-                    break;
-                case KeyEvent.VK_LEFT:
-                    playerX = Math.max(playerX - 1, 0);
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    playerX = Math.min(playerX + 1, gridSize - 1);
-                    break;
-            }
-            repaint();
+            localPlayer.move(e.getKeyCode());
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
 
         }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == timer) {
+                repaint();
+            }
+        }
     }
 
     public Foobar() {
         setTitle("Grid Game");
-        setSize((gridSize + 1) * cellSize, (gridSize + 1) * cellSize+1);
+        setSize((gridSize + 1) * cellSize, (gridSize + 1) * cellSize+1 + 50);
         setLayout(new BorderLayout());
         add(new Game(cellSize, gridSize));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
